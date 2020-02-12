e02e80ea93cd7b519a79de31f63a037a8adcef9d
EC2 Default User <ec2-user@ip-10-0-2-138.eu-central-1.compute.internal>
2020-02-12 13:47:00 +0000

    first commit

---
+++ menu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo `clear`
+SERVERS=('1a_private' '1a_public' '1b_private' '1b_public')
+
+echo "Server to connect:"
+for server in ${!SERVERS[*]}
+do
+    printf "%4d: %s\n" $server ${SERVERS[$server]}
+done
+
+read -p "Select a server to connect: " CHOISE
+
+read -p "Enter username (default user: ec2-user) : " USERNAME
+USERNAME=${USERNAME:-ec2-user}  # If username not set or null, use ec2-user.
+
+PEM=/home/ec2-user/environment/key-pair.pem
+
+read -r -p "Are you using ssh key? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    ssh -i $PEM $USERNAME@${SERVERS[$CHOISE]}
+else
+    ssh $USERNAME@${SERVERS[$CHOISE]}
+fi
